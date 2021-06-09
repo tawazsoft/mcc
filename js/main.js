000b001c7930e839e6b3895953e038a5c8cef5b1
Tawanda <tawanda.m@sproutcoding.org>
2021-06-09 10:06:33 +0200

    commit 2

--- js/main.js
+++ js/main.js
@@ -70,3 +70,14 @@ $('.button').click(function(){
   $(this).addClass('active');
 })
 
+
+const header=document.querySelector('.header')
+window .onscroll=function(){
+    var top= window.scrollY;
+    console.log(top);
+    if (top>=100){
+        header.classList.add('active')
+    }else{
+        header.classList.remove('active')
+    }
+}
